538eb42fe4ac5aa79285844f639431723dc2d230
Melkory <carloseduardo.dziombra@hotmail.com>
2025-01-17 17:57:09 -0300

    Relacionamento Category e Product

--- src/main/java/com/dziombra/consolidacao/entities/Product.java
+++ src/main/java/com/dziombra/consolidacao/entities/Product.java
@@ -2,6 +2,9 @@ package com.dziombra.consolidacao.entities;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "tb_product")
 public class Product {
@@ -13,6 +16,10 @@ public class Product {
     private String description;
     private Double price;
     private String imgUrl;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
 
     public Product() {
     }
