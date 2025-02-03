57b5dff9e6efac258029ee77c9b279731edf5d34
Melkory <carloseduardo.dziombra@hotmail.com>
2025-02-03 18:51:39 -0300

    Category Controller

---
+++ src/main/java/com/dziombra/consolidacao/controllers/CategoryController.java
@@ -0,0 +1,27 @@
+package com.dziombra.consolidacao.controllers;
+
+import com.dziombra.consolidacao.dto.CategoryDTO;
+import com.dziombra.consolidacao.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll () {
+        List<CategoryDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
+
+}
