09712f5cdd2a41d9f18a5603b2a377dcddaace01
Melkory <carloseduardo.dziombra@hotmail.com>
2025-02-04 17:10:15 -0300

    OrderService

---
+++ src/main/java/com/dziombra/consolidacao/services/OrderService.java
@@ -0,0 +1,22 @@
+package com.dziombra.consolidacao.services;
+
+import com.dziombra.consolidacao.dto.OrderDTO;
+import com.dziombra.consolidacao.entities.Order;
+import com.dziombra.consolidacao.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    @Transactional(readOnly = true)
+    public OrderDTO findById (Long id) {
+        Order order = repository.findById(id).get();
+        return new OrderDTO(order);
+    }
+
+}
